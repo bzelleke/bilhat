20542b3b48dd73ca874dfd2898f2059bfe054072
bzelleke <61984086+bzelleke@users.noreply.github.com>
2020-03-16 15:10:48 -0400

    Update EmployeeRepository.java

--- EmployeeRepository.java
+++ EmployeeRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bilhat.infosys.crudapp.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-	// that's it ... no need to write any code LOL!
+	//no need to write any code 
 
 }
 
